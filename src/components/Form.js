c7807196c98f7055f2be6b3f14d55b1a2184fa97
wisygig <jason.mcgibbon@gmail.com>
2018-12-16 21:18:05 -0500

    remove handleSubmit method from Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -13,7 +13,6 @@ class Form extends Component {
       this.state = {value: ''};
 
       this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
   }
 
 
@@ -22,24 +21,18 @@ class Form extends Component {
     this.setState({value: event.target.value});
   }
 
-  handleSubmit(event) {
-    event.preventDefault();
-    console.log('A name was submitted: ' + this.state.value);
-    this.props.sendDataToParent(this.state.value);
-  }
-
   render() {
     return (
       <>
-      <form onSubmit={this.handleSubmit}>
         <input
           type="text"
           value={this.state.value}
           onChange={this.handleChange} />
-        <input type="submit" value="Submit" />
-      </form>
-
-      <h2>child: {this.state.value}</h2>
+        <input
+          type="submit"
+          value="Update parent"
+          onClick={() => this.props.sendDataToParent(this.state.value)} />
+        <h2>child: {this.state.value}</h2>
       </>
     );
   }
